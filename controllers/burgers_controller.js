bfb5151d7eb040727a6e81dac7f88961045bf695
Emily Wirtz <ewirtz3@gmail.com>
2020-06-13 17:03:13 -0500

    creating views and layouts folders, handlebars files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,26 @@ const burger = require("../models/burger.js");
 
 const router = express.Router;
 
+router.get("/", (req, res) => {
+  console.log({ burgers: data });
+  res.render("index", { burgers: data });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burger.create([req.body.burger_name], (result) => {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/cats/:burger_name", (req, res) => {
+  console.log(req);
+  burger.update([req.body.burger_name], (result) => {
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
 module.exports = router;
