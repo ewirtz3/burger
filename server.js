b5e3b3b75882ecb8eedd4d77aa03f7d02bc05777
Emily Wirtz <ewirtz3@gmail.com>
2020-06-13 21:32:03 -0500

    connecting express server on server.js file

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening at localhost: ${PORT}`);
+});
